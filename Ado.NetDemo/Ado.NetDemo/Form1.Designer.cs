2c9f9759079ee3f999215617e8c13160af393dfb
Urwashi Makheja <162986350+Urwashi123@users.noreply.github.com>
2024-06-07 15:45:19 +0530

    Add files via upload

--- Ado.NetDemo/Ado.NetDemo/Form1.Designer.cs
+++ Ado.NetDemo/Ado.NetDemo/Form1.Designer.cs
@@ -42,12 +42,17 @@
             this.btnSelect = new System.Windows.Forms.Button();
             this.cmbCity = new System.Windows.Forms.ComboBox();
             this.cmbIsActive = new System.Windows.Forms.ComboBox();
+            this.lblTotalEmployees = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(229, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 20);
             this.label1.TabIndex = 0;
@@ -56,7 +61,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 103);
+            this.label2.Location = new System.Drawing.Point(438, 23);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 1;
@@ -65,7 +70,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(226, 26);
+            this.label3.Location = new System.Drawing.Point(12, 78);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 20);
             this.label3.TabIndex = 2;
@@ -74,7 +79,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(226, 100);
+            this.label4.Location = new System.Drawing.Point(229, 81);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 20);
             this.label4.TabIndex = 3;
@@ -83,7 +88,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(426, 32);
+            this.label5.Location = new System.Drawing.Point(438, 84);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 20);
             this.label5.TabIndex = 4;
@@ -91,57 +96,60 @@
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(103, 23);
+            this.txtFirstName.Location = new System.Drawing.Point(319, 20);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(100, 26);
             this.txtFirstName.TabIndex = 5;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(103, 100);
+            this.txtLastName.Location = new System.Drawing.Point(541, 19);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(100, 26);
             this.txtLastName.TabIndex = 6;
             // 
             // txtGender
             // 
-            this.txtGender.Location = new System.Drawing.Point(307, 22);
+            this.txtGender.Location = new System.Drawing.Point(103, 75);
             this.txtGender.Name = "txtGender";
             this.txtGender.Size = new System.Drawing.Size(100, 26);
             this.txtGender.TabIndex = 7;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(635, 25);
+            this.btnAdd.Location = new System.Drawing.Point(701, 23);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 27);
+            this.btnAdd.Size = new System.Drawing.Size(94, 38);
             this.btnAdd.TabIndex = 10;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(804, 22);
+            this.btnUpdate.Location = new System.Drawing.Point(860, 23);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 27);
+            this.btnUpdate.Size = new System.Drawing.Size(87, 41);
             this.btnUpdate.TabIndex = 11;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(635, 97);
+            this.btnDelete.Location = new System.Drawing.Point(701, 96);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 27);
+            this.btnDelete.Size = new System.Drawing.Size(94, 34);
             this.btnDelete.TabIndex = 12;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(804, 100);
+            this.btnSelect.Location = new System.Drawing.Point(851, 100);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(75, 27);
+            this.btnSelect.Size = new System.Drawing.Size(87, 34);
             this.btnSelect.TabIndex = 13;
             this.btnSelect.Text = "Select";
             this.btnSelect.UseVisualStyleBackColor = true;
@@ -150,7 +158,7 @@
             // cmbCity
             // 
             this.cmbCity.FormattingEnabled = true;
-            this.cmbCity.Location = new System.Drawing.Point(307, 103);
+            this.cmbCity.Location = new System.Drawing.Point(298, 78);
             this.cmbCity.Name = "cmbCity";
             this.cmbCity.Size = new System.Drawing.Size(121, 28);
             this.cmbCity.TabIndex = 14;
@@ -158,16 +166,54 @@
             // cmbIsActive
             // 
             this.cmbIsActive.FormattingEnabled = true;
-            this.cmbIsActive.Location = new System.Drawing.Point(497, 25);
+            this.cmbIsActive.Location = new System.Drawing.Point(520, 81);
             this.cmbIsActive.Name = "cmbIsActive";
             this.cmbIsActive.Size = new System.Drawing.Size(121, 28);
             this.cmbIsActive.TabIndex = 15;
+            // 
+            // lblTotalEmployees
+            // 
+            this.lblTotalEmployees.AutoSize = true;
+            this.lblTotalEmployees.Location = new System.Drawing.Point(24, 182);
+            this.lblTotalEmployees.Name = "lblTotalEmployees";
+            this.lblTotalEmployees.Size = new System.Drawing.Size(0, 20);
+            this.lblTotalEmployees.TabIndex = 16;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 205);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(958, 233);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(52, 22);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(23, 20);
+            this.lblId.TabIndex = 18;
+            this.lblId.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(103, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 26);
+            this.txtId.TabIndex = 19;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(982, 450);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblTotalEmployees);
             this.Controls.Add(this.cmbIsActive);
             this.Controls.Add(this.cmbCity);
             this.Controls.Add(this.btnSelect);
@@ -184,6 +230,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,5 +252,9 @@
         private System.Windows.Forms.Button btnSelect;
         private System.Windows.Forms.ComboBox cmbCity;
         private System.Windows.Forms.ComboBox cmbIsActive;
+        private System.Windows.Forms.Label lblTotalEmployees;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
     }
 }
--- Ado.NetDemo/Ado.NetDemo/Form1.cs
+++ Ado.NetDemo/Ado.NetDemo/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,9 @@ namespace Ado.NetDemo
             InitializeComponent();
             populateCities();
             populateEmployeeStatus();
+            GetCountofEmployees();
         }
+        //Execute Reader
         private void populateCities()
         {
             SqlConnection con = new SqlConnection(ConStr);
@@ -48,6 +51,7 @@ namespace Ado.NetDemo
             }
 
         }
+        //Execute Reader
         private void populateEmployeeStatus()
         {
             SqlConnection con = new SqlConnection(ConStr); 
@@ -75,24 +79,135 @@ namespace Ado.NetDemo
             }
 
         }
+        //Execute scalar
+        private void GetCountofEmployees()
+        {
+            SqlConnection con=new SqlConnection(ConStr);
+            try
+            {
+                string GetEmployeeCountSql = "select Count(*) As 'Total Employee' from Emp;";
+                SqlCommand cmd = new SqlCommand(GetEmployeeCountSql, con);
+                con.Open();
+                int EmployeeCount=Convert.ToInt32(cmd.ExecuteScalar());
+                if(EmployeeCount > 0)
+                {
+                    lblTotalEmployees.Text = "Total num of employee present is " + EmployeeCount;
+                }
+                else
+                {
+                    lblTotalEmployees.Text = "No employee hired yet";
+                }
+            }
+            catch(Exception ex)
+
+            {
+                MessageBox.Show(ex.Message );
+            }
+            finally
+            {
+              con.Close();
+            }
+        }
+        private void FillDataGridView() 
+        {
+           // SqlConnection con = new SqlConnection(ConStr);
+            try
+            {
+                string getAllEmployeeSql = "select * from Emp";
+                SqlDataAdapter da = new SqlDataAdapter(getAllEmployeeSql, ConStr);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+                //string getEmployeeSql = "[dbo].[GetEmployees]";
+                //SqlCommand cmd = new SqlCommand(getEmployeeSql, con);
+                //cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.CommandText = getEmployeeSql;
+                //SqlDataAdapter da = new SqlDataAdapter(cmd);
+                //DataSet ds= new DataSet();
+                //da.Fill(ds);
+                //dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //con.Close();
+            }
+            
+        }
+        //Execute Reader
         private void btnSelect_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(ConStr);
-            string SqlQuery = "Select * from [dbo].[Emp]";
+           
+          
+                string SqlQuery = "Select * from [dbo].[Emp] where Id=@Id";
 
+                try
+                {
+                    if (!string.IsNullOrEmpty(txtId.Text)) { 
+                        SqlCommand cmd = new SqlCommand(SqlQuery, con);
+                    cmd.Parameters.AddWithValue("@Id", txtId.Text);
+
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        txtFirstName.Text = reader.GetString(1);
+                        txtLastName.Text = reader.GetString(2);
+                        txtGender.Text = reader.GetString(3);
+                        cmbCity.Text = reader.GetString(4);
+                        cmbIsActive.Text = reader.GetString(5);
+
+                    }
+
+                }
+
+
+
+            else {
+                    MessageBox.Show("Employee cannot be left Blank");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+           
+        }
+        
+        //Execute Nonquery
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConStr);
             try
             {
-                SqlCommand cmd = new SqlCommand(SqlQuery, con);
+                string addEmployeeSql = @"Insert into Emp Values(@FirstName,@LastName,@Gender,@City,@IsActive)";
+                SqlCommand cmd = new SqlCommand(addEmployeeSql, con);
+                 cmd.Parameters.AddWithValue("@FirstName",txtFirstName.Text);
+                 cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                 cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+                 cmd.Parameters.AddWithValue("@City", cmbCity.SelectedValue);
+                 cmd.Parameters.AddWithValue("@IsActive", cmbIsActive.SelectedValue);
                 con.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
+                int rowAffected = cmd.ExecuteNonQuery();
+                if (rowAffected > 0)
                 {
-                    txtFirstName.Text = reader.GetString(1);
-                    txtLastName.Text = reader.GetString(2);
-                    txtGender.Text = reader.GetString(3);
-                    //txtCity.Text = reader.GetString(4);
-                    //txtIsActive.Text = reader.GetString(5);
-
+                    MessageBox.Show("Record inserted Sucessfully");
+                    GetCountofEmployees();
+                    FillDataGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Record not inserted");
                 }
             }
             catch(Exception ex)
@@ -105,5 +220,78 @@ namespace Ado.NetDemo
             }
            
         }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConStr); 
+            try
+            {
+                string UpdateEmpSql = @"Update Emp Set [FirstName]=@FirstName,
+                                                         [LastName]=@LastName,
+                                                            [Gender]=@Gender,
+                                                               [City]=@City,
+                                                        [IsActive]=@IsActive where Id=@ID";
+                SqlCommand cmd = new SqlCommand(UpdateEmpSql, con);
+                cmd.Parameters.AddWithValue("@Id", txtId.Text);
+                cmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                cmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                cmd.Parameters.AddWithValue("@Gender", txtGender.Text);
+                cmd.Parameters.AddWithValue("@City", cmbCity.Text); 
+                cmd.Parameters.AddWithValue("@IsActive", cmbIsActive.Text);
+                con.Open();
+                int rowAffected = cmd.ExecuteNonQuery();
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Record updated sucessfully");
+                    FillDataGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Record not updated sucessfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConStr);
+            try
+            {
+                string DeleteEmpSql = @"Delete from Emp  where Id=@ID";
+                SqlCommand cmd = new SqlCommand(DeleteEmpSql, con);
+                cmd.Parameters.AddWithValue("@Id", txtId.Text);
+                
+                con.Open();
+                int rowAffected = cmd.ExecuteNonQuery();
+                if (rowAffected > 0)
+                {
+                    MessageBox.Show("Record deleted sucessfully");
+                    FillDataGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Record not deleted sucessfully");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        
+
+
     }
+}
 }
